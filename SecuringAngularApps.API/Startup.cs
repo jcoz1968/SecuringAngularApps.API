f2512325a349a0077cd0c7cea4a229c544db0880
Jeffery Cosby <jcoz1968@hotmail.com>
2018-07-31 20:36:49 -0500

    add authorization to api

--- SecuringAngularApps.API/Startup.cs
+++ SecuringAngularApps.API/Startup.cs
@@ -34,6 +34,13 @@ namespace SecuringAngularApps.API
                     .AllowCredentials();
                 });
             });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.Authority = "http://localhost:4242/";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "projects-api";
+                });
             services.AddMvc();
         }
 
@@ -44,6 +51,7 @@ namespace SecuringAngularApps.API
                 app.UseDeveloperExceptionPage();
             }
             app.UseCors("AllRequests");
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
